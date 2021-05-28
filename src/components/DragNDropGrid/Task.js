61119e767cad33ed93b005477d309eb9f9257ad0
Luis Pohren <luis.pohren@sap.com>
2021-05-28 14:39:58 -0300

    feat: remove cards fixed width

--- src/components/DragNDropGrid/Task.js
+++ src/components/DragNDropGrid/Task.js
@@ -4,13 +4,10 @@ import styled from 'styled-components';
 const Container = styled.div`
   margin: 2px;
   padding: 8px;
-
-  width: 80px;
   height: 40px;
   background-color: ${(props) => (props.isDragging ? 'lightgreen' : '#555278')};
   color: white;
   font-size: 12px;
-  //border-right: 6px solid black;
   border-right: 6px solid ${(props) => borderColor(props)};
   border-radius: 6px;
 `;
