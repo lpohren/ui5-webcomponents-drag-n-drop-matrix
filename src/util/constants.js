075e505dbf5bb6f3b27a303a616f084b5461b2ab
Luis Pohren <luis.pohren@sap.com>
2021-05-31 21:59:23 -0300

    feat: add multiple color options

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,6 +1,58 @@
 // Purple old
-export const STYLES = {
+export const PURPLE = {
   CARD_COLOR_PRIMARY: '#555278',
   CARD_COLOR_MARKED: 'lightgreen',
   CARD_FONT_COLOR: 'white',
 };
+
+//////////////////////////////////////////////////////
+///////// BELIZE
+//////////////////////////////////////////////////////
+
+// Belize light blue
+export const BELIZE_LIGHT_BLUE = {
+  CARD_COLOR_PRIMARY: '#0092D1',
+  CARD_COLOR_MARKED: '#2B7D2B',
+  CARD_FONT_COLOR: 'white',
+};
+
+// Belize Standard primary color
+export const BELIZE_STANDARD_PRIMARY = {
+  CARD_COLOR_PRIMARY: '#427CAC',
+  CARD_COLOR_MARKED: '#2B7D2B',
+  CARD_FONT_COLOR: 'white',
+};
+
+//////////////////////////////////////////////////////
+///////// DEFAULT WHITE FONT
+//////////////////////////////////////////////////////
+
+export const DEFAULT_PRIMARY = {
+  CARD_COLOR_PRIMARY: '#0a6ed1',
+  CARD_COLOR_MARKED: '#107e3e',
+  CARD_FONT_COLOR: 'white',
+};
+
+export const DEFAULT_GLOBAL_DARK = {
+  CARD_COLOR_PRIMARY: '#3F5161',
+  CARD_COLOR_MARKED: '#107e3e',
+  CARD_FONT_COLOR: 'white',
+};
+
+export const DEFAULT_ACCENT6 = {
+  CARD_COLOR_PRIMARY: '#286eb4',
+  CARD_COLOR_MARKED: '#107e3e',
+  CARD_FONT_COLOR: 'white',
+};
+
+//////////////////////////////////////////////////////
+///////// DEFAULT BLACK FONT
+//////////////////////////////////////////////////////
+
+export const DEFAULT_ACCENT4 = {
+  CARD_COLOR_PRIMARY: '#8CA7D5',
+  CARD_COLOR_MARKED: '#107e3e',
+  CARD_FONT_COLOR: 'black',
+};
+
+export const STYLES = BELIZE_STANDARD_PRIMARY;
